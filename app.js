ed485a9d96c7df28ee256ba6b7506c0bda9a38c5
I501475 <maud.kusters@student.fontys.nl>
2023-12-14 15:51:50 +0100

    step 6

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const express = require('express'); 
-const app = express();
-const blogRoutes = require('C:/Users/under/Desktop/FHICT/SEM3/FED/blogapp/routes/blogRoutes.js');
-const db = require('./database'); // Ensure this path is correct to your database setup file
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
 
+// Passport configuration
+7
+require('./passport-config')(passport);
+const app = express();
+const routes = require('./routes/routes'); // Combined routes file
+
+// Set view engine
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true })); // Body parser for form data
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+// Express session
+app.use(session({
+secret: 'secret',
+resave: false,
+saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables for flash messages
+app.use((req, res, next) => {
+res.locals.success_msg = req.flash('success_msg');
+res.locals.error_msg = req.flash('error_msg');
+res.locals.error = req.flash('error');
+next();
+});
 
 // Routes
-app.get('/', (req, res) => res.redirect('/blogs')); // Redirect root to the blogs
-app.use('/blogs', blogRoutes);
-
+app.use('/', routes);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
