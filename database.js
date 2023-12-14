01124765a1ee2ff0b8165ec57c71aa8d8f11933a
I501475 <maud.kusters@student.fontys.nl>
2023-12-14 15:40:56 +0100

    step 2

--- database.js
+++ database.js
@@ -7,8 +7,19 @@ console.log('Connected to the SQLite database.');
 });
 
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL,
-    content TEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`);
-    });
+    db.run(`CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`);
+ 
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )`);
+});
 
 module.exports = db;
